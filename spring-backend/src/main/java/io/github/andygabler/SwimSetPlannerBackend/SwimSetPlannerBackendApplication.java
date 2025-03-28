15563ed4901cf0276dc86f4e868b40408587ec94
AndyGabler <andrewpgabler@gmail.com>
2025-03-27 21:15:13 -0400

    and let's get a docker containerized spring app for me to use for my AWS ECR so I can deploy to EKS

---
+++ spring-backend/src/main/java/io/github/andygabler/SwimSetPlannerBackend/SwimSetPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.andygabler.SwimSetPlannerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimSetPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimSetPlannerBackendApplication.class, args);
+	}
+
+}

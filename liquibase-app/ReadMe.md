dfd777a9e909f67cc3b63fc61bebd54222879862
AndyGabler <andrewpgabler@gmail.com>
2025-06-18 21:47:14 -0400

    start of the liquibase app to get my aurora database setup.

---
+++ liquibase-app/ReadMe.md
@@ -0,0 +1,9 @@
+# Liquibase App
+
+Liquibase application that is used to generate the PostgresSQL database hosted and managed by AWS Aurora.
+
+## Local Setup
+
+Locally, you will need to add a gradle.properties file.
+
+This file is ignored in Git. This is used to configure the connection details to your local PostgresSQL instance.
---
+++ liquibase-app/src/main/java/io/github/andygabler/swimsetplannerdatabase/SwimSetPlannerDatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.andygabler.swimsetplannerdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimSetPlannerDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimSetPlannerDatabaseApplication.class, args);
+	}
+
+}
